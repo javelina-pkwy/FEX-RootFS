1ad3797b5e562c6e9a4c027df8ddd5b3d367f48a
Justin Becker <jubecker@nvidia.com>
2026-09-03 15:32:13 -0700

    Progress on adding 26.04

---
+++ Scripts/Ubuntu_26_04/build_install_mesa.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Add source packages
+sed -i -e "s/^Types: deb/Types: deb deb-src/g" /etc/apt/sources.list.d/ubuntu.sources
+apt-get update
+
+cd /root
+export DEBIAN_FRONTEND=noninteractive
+dpkg --add-architecture i386
+
+apt-get install -y software-properties-common
+
+# Just use kisk-ppa
+# Debian/Ubuntu llvm development packages have been broken for multiple releases.
+add-apt-repository -y ppa:kisak/kisak-mesa
+
+apt-get update
+apt install -y libgl1-mesa-dri libgl1-mesa-dri:i386
+
+apt-get upgrade -y
+apt-get install -y libgl1 libgl1:i386 mesa-va-drivers mesa-vdpau-drivers mesa-vulkan-drivers libglx-mesa0 libgl1-mesa-dri \
+    libegl-mesa0
+
+apt-get upgrade -y
+
+apt-mark hold \
+  libegl-mesa0 libegl-mesa0:i386 libgbm1 libgbm1:i386 libgl1-mesa-dri \
+  libgl1-mesa-dri:i386 libglx-mesa0 libglx-mesa0:i386 libllvm21 libllvm21:i386 \
+  mesa-libgallium mesa-libgallium:i386 mesa-va-drivers mesa-va-drivers:i386 \
+  mesa-vdpau-drivers mesa-vdpau-drivers:i386 mesa-vulkan-drivers \
+  mesa-vulkan-drivers:i386 libgl1 libgl1:i386
